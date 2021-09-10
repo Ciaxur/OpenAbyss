d23c3ec7a6f53db48e3ab3876a4bc8679cde299b
Ciaxur <oomar@umich.edu>
2021-09-09 21:37:04 -0400

    Add SIGINT handler

--- utils/handlers.go
+++ utils/handlers.go
@@ -1,9 +1,23 @@
 package utils
 
-import "log"
+import (
+	"log"
+	"os"
+)
+
+func IsErrorSIGINT(err error) bool {
+	if err != nil {
+		return err.Error() == "^C"
+	}
+	return false
+}
 
 func HandleErr(err error, msg string) {
 	if err != nil {
+		if IsErrorSIGINT(err) {
+			log.Fatalln("Unhandled SIGINT")
+			os.Exit(1)
+		}
 		log.Panicln(msg, err)
 	}
 }
