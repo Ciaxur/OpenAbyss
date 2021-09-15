c93160a04611527a9d926dc5c58254a2bcf08353
Omar Omar <oomar@umich.edu>
2021-09-15 19:07:53 -0400

    💪 Improve Storage Test
    - Better fail message
    - Add Recursive sub-storage test

--- tests/storage/storage_test.go
+++ tests/storage/storage_test.go
@@ -18,18 +18,18 @@ func TestFileStorage_Store_RootFile_Success(t *testing.T) {
 
 	err := storage.Internal.Store(hexFileId, filePath, storage.Type_File)
 
-	assert.Nil(t, err, "internal store should not have failed")
-	assert.Greater(t, len(storage.Internal.Storage), 0, "internal storage should contain data")
+	assert.Nil(t, err, "internal store failed")
+	assert.Greater(t, len(storage.Internal.Storage), 0, "internal storage does not contain data")
 
 	fStorage, ok := storage.Internal.Storage[path.Base(filePath)]
 	assert.True(t, ok, "file not stored in root storage")
 
 	// Check Storage Entries
-	assert.Greater(t, fStorage.CreatedAt_UnixTimestamp, uint64(0), "filestorage should have a created timestamp")
-	assert.Greater(t, fStorage.ModifiedAt_UnixTimestamp, uint64(0), "filestorage should have a modified timestamp")
-	assert.Equal(t, storage.Type_File, fStorage.Type, "stored file should be a File Type")
-	assert.Equal(t, hexFileId, fStorage.Name, "stored assigned filename should match SHA256 Sum")
-	assert.Equal(t, filePath, fStorage.Path, "stored file should contain the client's full path")
+	assert.Greater(t, fStorage.CreatedAt_UnixTimestamp, uint64(0), "filestorage did not create created timestamp")
+	assert.Greater(t, fStorage.ModifiedAt_UnixTimestamp, uint64(0), "filestorage did not create modified timestamp")
+	assert.Equal(t, storage.Type_File, fStorage.Type, "stored file is not a File Type")
+	assert.Equal(t, hexFileId, fStorage.Name, "stored assigned filename does not match SHA256 Sum")
+	assert.Equal(t, filePath, fStorage.Path, "stored file does not contain the client's full path")
 }
 
 func TestFileStorage_Store_RecursiveSubDir_Success(t *testing.T) {
@@ -40,47 +40,47 @@ func TestFileStorage_Store_RecursiveSubDir_Success(t *testing.T) {
 
 	err := storage.Internal.Store(hexFileId, filePath, storage.Type_File)
 
-	assert.Nil(t, err, "internal store should not have failed")
-	assert.Greater(t, len(storage.Internal.StorageMap), 0, "internal storage map should contain data")
+	assert.Nil(t, err, "internal store failed")
+	assert.Greater(t, len(storage.Internal.StorageMap), 0, "internal storage map does not contain data")
 
 	// Root Level should contain "path" sub-storage
 	rootSubStorage := storage.Internal.GetSubStorage("path")
-	assert.NotNil(t, rootSubStorage, "path should be the root sub-storage")
-	assert.NotNil(t, rootSubStorage.Storage, "root storage should be not be nil")
+	assert.NotNil(t, rootSubStorage, "path is not the root sub-storage")
+	assert.NotNil(t, rootSubStorage.Storage, "root storage failed to create")
 
 	// Check metadata
-	assert.Greater(t, rootSubStorage.CreatedAt_UnixTimestamp, uint64(0), "root filestorage should have a created timestamp")
-	assert.Greater(t, rootSubStorage.ModifiedAt_UnixTimestamp, uint64(0), "root filestorage should have a modified timestamp")
+	assert.Greater(t, rootSubStorage.CreatedAt_UnixTimestamp, uint64(0), "root filestorage did not create created timestamp")
+	assert.Greater(t, rootSubStorage.ModifiedAt_UnixTimestamp, uint64(0), "root filestorage did not create modified timestamp")
 
 	// 2nd-Level Storage
 	secLvlStorage := rootSubStorage.GetSubStorage("to")
 	assert.NotNil(t, secLvlStorage, "2nd level should be the 'to' sub-storage")
-	assert.NotNil(t, secLvlStorage.Storage, "2nd level storage should not be nil")
+	assert.NotNil(t, secLvlStorage.Storage, "2nd level storage failed to create")
 
 	// Check metadata
-	assert.Greater(t, secLvlStorage.CreatedAt_UnixTimestamp, uint64(0), "2nd level filestorage should have a created timestamp")
-	assert.Greater(t, secLvlStorage.ModifiedAt_UnixTimestamp, uint64(0), "2nd level filestorage should have a modified timestamp")
+	assert.Greater(t, secLvlStorage.CreatedAt_UnixTimestamp, uint64(0), "2nd level filestorage did not create created timestamp")
+	assert.Greater(t, secLvlStorage.ModifiedAt_UnixTimestamp, uint64(0), "2nd level filestorage did not create modified timestamp")
 
 	// 3rd-Level Storage
 	thirdLvlStorage := secLvlStorage.GetSubStorage("file")
-	assert.Nil(t, thirdLvlStorage, "3rd level should be nil, since there is not sub-storage there")
+	assert.Nil(t, thirdLvlStorage, "3rd level contains sub-storage")
 
 	// Get storage file from 2nd level
-	assert.Greater(t, len(secLvlStorage.Storage), 0, "2nd level should contain storage")
+	assert.Greater(t, len(secLvlStorage.Storage), 0, "2nd level does not contain storage")
 	storageFile := secLvlStorage.GetStorage("file")
-	assert.NotNil(t, storageFile, "2nd level should return storage file")
+	assert.NotNil(t, storageFile, "2nd level did not return storage file")
 
 	// Check metadata
-	assert.Greater(t, storageFile.CreatedAt_UnixTimestamp, uint64(0), "basename filestorage should have a created timestamp")
-	assert.Greater(t, storageFile.ModifiedAt_UnixTimestamp, uint64(0), "basename filestorage should have a modified timestamp")
+	assert.Greater(t, storageFile.CreatedAt_UnixTimestamp, uint64(0), "basename filestorage did not create created timestamp")
+	assert.Greater(t, storageFile.ModifiedAt_UnixTimestamp, uint64(0), "basename filestorage did not create modified timestamp")
 
 	// Check file-specific metadata
-	assert.Equal(t, storage.Type_File, storageFile.Type, "stored file should be a File Type")
-	assert.Equal(t, hexFileId, storageFile.Name, "stored assigned filename should match SHA256 Sum")
-	assert.Equal(t, filePath, storageFile.Path, "stored file should contain the client's full path")
+	assert.Equal(t, storage.Type_File, storageFile.Type, "stored file is not a File Type")
+	assert.Equal(t, hexFileId, storageFile.Name, "stored assigned filename does not match SHA256 Sum")
+	assert.Equal(t, filePath, storageFile.Path, "stored file does not contain the client's full path")
 }
 
-func TestFileStorage_Store_GetRootFile_Success(t *testing.T) {
+func TestFileStorage_Store_GetRecursiveSubStorage_Success(t *testing.T) {
 	filePath := "/path/to/file"
 	fileId := sha256.Sum256([]byte(filePath))
 	hexFileId := hex.EncodeToString(fileId[:])
@@ -89,14 +89,38 @@ func TestFileStorage_Store_GetRootFile_Success(t *testing.T) {
 	// Store file internally
 	err := storage.Internal.Store(hexFileId, filePath, storage.Type_File)
 
-	assert.Nil(t, err, "internal store should not have failed")
-	assert.Greater(t, len(storage.Internal.StorageMap), 0, "internal storage map should contain data")
+	assert.Nil(t, err, "internal store failed")
+	assert.Greater(t, len(storage.Internal.StorageMap), 0, "internal storage map does not contain data")
 
 	// Fetch file by path
 	fileStorage, err := storage.Internal.GetFileByPath(filePath)
 
-	assert.Nil(t, err, "internal storage GetFileByPath should not fail")
-	assert.NotNil(t, fileStorage, "filestorage should contain file storage")
+	assert.Nil(t, err, "fetching file by path failed")
+	assert.NotNil(t, fileStorage, "fetching filestorage failed")
+
+	// Validate FileStorage correctness
+	assert.Equal(t, filePath, fileStorage.Path, "file storage meta: path does not match")
+	assert.Equal(t, hexFileId, fileStorage.Name, "file storage meta: fileId does not match")
+	assert.Equal(t, storage.Type_File, fileStorage.Type, "file storage meta: file type does not match")
+}
+
+func TestFileStorage_Store_GetRootFile_Success(t *testing.T) {
+	filePath := "/file"
+	fileId := sha256.Sum256([]byte(filePath))
+	hexFileId := hex.EncodeToString(fileId[:])
+	storage.Internal = storage.FileStorageMap{}
+
+	// Store file internally
+	err := storage.Internal.Store(hexFileId, filePath, storage.Type_File)
+
+	assert.Nil(t, err, "internal store failed")
+	assert.Greater(t, len(storage.Internal.Storage), 0, "internal storage does not contain data")
+
+	// Fetch file by path
+	fileStorage, err := storage.Internal.GetFileByPath(filePath)
+
+	assert.Nil(t, err, "fetching file by path failed")
+	assert.NotNil(t, fileStorage, "fetching filestorage failed")
 
 	// Validate FileStorage correctness
 	assert.Equal(t, filePath, fileStorage.Path, "file storage meta: path does not match")
