bfdd2f603b919876333bbcc3fa1e2139f7467238
Omar Omar <oomar@umich.edu>
2021-10-08 20:10:21 -0700

    📦 Add backup configuration

--- server/configuration/env.go
+++ server/configuration/env.go
@@ -1,13 +1,20 @@
 package configuration
 
-// Configuraiton Structure
+// Periodic Backup Sub-config
+type BackupSubConfiguration struct {
+	RetentionPeriod uint64 `json:"retentionPeriod"` // Retention Period for backups in Milliseconds
+	BackupFrequency uint64 `json:"backupFrequency"` // How frequently to backup in Milliseconds
+}
+
+// Root Configuraiton Structure
 type Configuration struct {
-	DefaultKeyAlgorithm string `json:"defaultKeyAlgorithm"`
-	Insecure            bool   `json:"insecure"`
-	GrpcPort            uint16 `json:"grpcPort"`
-	GrpcHost            string `json:"grpcHost"`
-	TLSCertPath         string `json:"tlsCertPath"`
-	TLSKeyPath          string `json:"tlsKeyPath"`
+	DefaultKeyAlgorithm string                 `json:"defaultKeyAlgorithm"`
+	Insecure            bool                   `json:"insecure"`
+	GrpcPort            uint16                 `json:"grpcPort"`
+	GrpcHost            string                 `json:"grpcHost"`
+	TLSCertPath         string                 `json:"tlsCertPath"`
+	TLSKeyPath          string                 `json:"tlsKeyPath"`
+	Backup              BackupSubConfiguration `json:"backup"`
 }
 
 // Assigned Default Values
@@ -19,6 +26,10 @@ var (
 		GrpcPort:            50051,
 		TLSCertPath:         "cert/server.crt",
 		TLSKeyPath:          "cert/server.key",
+		Backup: BackupSubConfiguration{
+			RetentionPeriod: 7 * 24 * 60 * 60 * 1000, // 7 Days by default
+			BackupFrequency: 7 * 24 * 60 * 60 * 1000, // Daily backups by default
+		},
 	}
 	InternalConfigDirPath string = ".config"            // Directory that holds configs
 	ConfigFileName        string = "config-server.json" // Configuration JSON Filename
